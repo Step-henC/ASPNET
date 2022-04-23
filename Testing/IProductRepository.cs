4a9b7503dd8864392b0d99fae7b9eb6caa4cf031
Stephen Cunningham <stevecunningham@outlook.com>
2022-04-23 15:38:39 -0500

    completed commit

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProduct();
+        public Product GetProduct(int id);
+
+        public void UpdateProduct(Product product);
+
+        public void InsertProduct(Product productToInsert);
+
+        public IEnumerable<Category> GetCategories();
+
+        public Product AssignCategory();
+
+        public void DeleteProduct(Product product);
+    }
+}
---
+++ Testing/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using Testing.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Testing.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int CategoryID { get; set; }
+        public int OnSale { get; set; }
+        public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
